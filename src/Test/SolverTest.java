1cb8d7d9ef6af6334f4973f92d0cbe59d69496ac
FredrikHessner <Fredrik@hessner.se>
2020-11-24 16:23:02 +0100

    Created Junit package and test

---
+++ src/Test/SolverTest.java
@@ -0,0 +1,17 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import Sudoku.Solver;
+
+class SolverTest {
+	private Solver sudokuBoard;
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
