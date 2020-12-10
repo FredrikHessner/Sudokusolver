4c06f6d312ec070a133f06ef5598f75ebcfbad09
Joel Jensen <info@joeljensen.se>
2020-12-10 10:44:25 +0100

    Update comments

--- src/Sudoku/SudokuSolver.java
+++ src/Sudoku/SudokuSolver.java
@@ -10,39 +10,56 @@ public interface SudokuSolver {
 	 * @param row    The row
 	 * @param col    The column
 	 * @param number The digit to insert
-	 * @throws IllegalArgumentException if number not in [1..9] or row or col is outside the allowed range
+	 * @throws IllegalArgumentException if number not in [0..8] or row or col is outside the allowed range
 	 */
 	void setNumber(int row, int col, int number);
 	
-	/** Kollar om siffran number kan sÃ¤ttas i raden row och kolumnen col, 
-	 * om det inte går enligt spelreglerna returneras false 
-	 * @throws IllegalArgumentException if number not in [1..9] or row or col is outside the allowed range
+	/**
+	 * Checks weather number can be put in the row & col, return false if not
+	 * 
+	 * @param row    The row
+	 * @param col    The column
+	 * @param number The digit to insert
+	 * @throws IllegalArgumentException if number not in [0..8] or row or col is outside the allowed range
 	 */
 	boolean trySetNumber(int row, int col, int number);
 	
-	/** Returnerar siffran på raden row och kolumnen col. 
-	 * @throws IllegalArgumentException if row or col is outside the allowed range
+	/**
+	 * Returns the number in row - col
+	 * 
+	 * @param row    The row
+	 * @param col    The column
+	 * @return Integer The digit in row - col
+	 * @throws IllegalArgumentException if row or col is outside the allowed range [0..8]
 	 */
 	int getNumber(int row, int col);
 	
-	/** Tar bort siffran på raden row och kolumnen col. 
-	 * @throws IllegalArgumentException if row or col is outside the allowed range
+	/**
+	 * Removes the number in row - col
+	 * 
+	 * @param row    The row
+	 * @param col    The column
+	 * @throws IllegalArgumentException if row or col is outside the allowed range [0..8]
 	 */
 	void removeNumber(int row, int col);
 
-	/** Löser sudokut och returnerar true om sudokut går att läsa. */
+	/**
+	 * Solves the sudoku and returns true if possible
+	 */
 	boolean solve();
 	
-	/** Returnerar siffrorna i sudokut. */
+	/**
+	 * Returns all the numbers in the sudoku grid
+	 * 
+	 * @return Integer[][] All numbers in grid 9x9
+	 */
 	int[][] getNumbers();
 	
-	/** Fyller i siffrorna i numbers i sudokut. 
-	 * @throws IllegalArgumentException if not all numbers in [0..9]
+	/**
+	 * Set the numbers in the grid
+	 * 
+	 * @param int[][] 	The numbers in the grid
+	 * @throws IllegalArgumentException if not all numbers in [0..8]
 	 **/
 	void setNumbers(int[][] numbers);
 }
-
-/* TBD:
- * Numrering av row och col: 0-8 eller 1-9? Senaste förslag 0-8
- * Skriva javadoc-kommentarer (Påbörjat, kommentaren till setNumber är nästan klar.)
- */
